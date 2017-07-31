2242a9246c0471e579fee83b2680147e0714a6eb
Clare <clarelpidgeon@yahoo.co.uk>
2017-07-31 15:17:19 +0100

    axis update

--- static/js/graph.js
+++ static/js/graph.js
@@ -110,7 +110,7 @@ var line = d3.svg.line()
 	.y(function(d){return y(d);});
 
 var graph = d3.select("#netChart").append("svg:svg")
-	.attr("width", "330")
+	.attr("width", "360")
 	.attr("height", "200")
 	.append("svg:g");
 
@@ -121,7 +121,7 @@ graph.append("svg:g")
 	.attr("class", "x axis")
 	.call(xAxis);
 
-var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left").style("font", "10px");
+var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
 
 graph.append("svg:g")
 	.attr("class", "y axis")
@@ -129,7 +129,7 @@ graph.append("svg:g")
 
 graph.append("svg:path").attr("d", line(netTransfersBySeason));
    netChart
-       .width(330)
+       .width(360)
        .height(200)
        .dimension(seasonDim)
        .group(netTransfersBySeason)
